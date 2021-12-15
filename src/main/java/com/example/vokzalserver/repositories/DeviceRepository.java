0f00a043ac2a73f701a11983cde29c5325d16748
TumbaevAA <theonetommygun1@gmail.com>
2021-12-15 17:33:45 +0700

    Add DeviceRepository for sending SMSDeviceJson

---
+++ src/main/java/com/example/vokzalserver/repositories/DeviceRepository.java
@@ -0,0 +1,15 @@
+package com.example.vokzalserver.repositories;
+
+import com.example.vokzalserver.entities.DeviceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DeviceRepository extends JpaRepository<DeviceEntity,Long> {
+
+    @Query("select d from DeviceEntity d where d.type = ?1")
+    List<DeviceEntity> findAllByType(String type);
+}
