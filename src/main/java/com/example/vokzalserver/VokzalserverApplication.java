f2965eb3f6c6737bca35121490905e52927c9a65
TumbaevAA <theonetommygun1@gmail.com>
2021-12-15 17:15:52 +0700

    Initial commit

---
+++ src/main/java/com/example/vokzalserver/VokzalserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.vokzalserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VokzalserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VokzalserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vokzalserver/VokzalserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vokzalserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VokzalserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
